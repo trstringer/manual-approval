3722c4cc691b7b370d1b0a374ea105ff9ccfaef4
Timothy Ng <timothy@leaselock.com>
2022-11-08 12:18:02 -0800

    Add constant for GITHUB_ACTOR env var

--- constants.go
+++ constants.go
@@ -5,13 +5,14 @@ import "time"
 const (
 	pollingInterval time.Duration = 10 * time.Second
 
-	envVarRepoFullName     string = "GITHUB_REPOSITORY"
-	envVarRunID            string = "GITHUB_RUN_ID"
-	envVarRepoOwner        string = "GITHUB_REPOSITORY_OWNER"
-	envVarToken            string = "INPUT_SECRET"
-	envVarApprovers        string = "INPUT_APPROVERS"
-	envVarMinimumApprovals string = "INPUT_MINIMUM-APPROVALS"
-	envVarIssueTitle       string = "INPUT_ISSUE-TITLE"
+	envVarRepoFullName      string = "GITHUB_REPOSITORY"
+	envVarRunID             string = "GITHUB_RUN_ID"
+	envVarRepoOwner         string = "GITHUB_REPOSITORY_OWNER"
+	envVarWorkflowInitiator string = "GITHUB_ACTOR"
+	envVarToken             string = "INPUT_SECRET"
+	envVarApprovers         string = "INPUT_APPROVERS"
+	envVarMinimumApprovals  string = "INPUT_MINIMUM-APPROVALS"
+	envVarIssueTitle        string = "INPUT_ISSUE-TITLE"
 )
 
 var (
